edcd4d7a51bbbf6214f6431e1baaa680fc1c48d6
Dylan Sbogar <dylansbogar@gmail.com>
2023-12-03 01:47:55 +1100

    Updated index.js to read in all command files from /commands folder, and created deployCommands.js to deploy all files in said folder.

---
+++ commands/test.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("test")
+    .setDescription("testing bot reading slash commands"),
+  async execute(interaction) {
+    await interaction.reply("response!");
+  },
+};
