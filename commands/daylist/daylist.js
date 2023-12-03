8272ed162e32f4bf7480c0c3f83fe96a11c4e202
Dylan Sbogar <dylansbogar@gmail.com>
2023-12-03 17:55:57 +1100

    Added support for strings in daylist command. Still need to have reading/writing of a text file to see whats new and what isnt

--- commands/daylist/daylist.js
+++ commands/daylist/daylist.js
@@ -3,75 +3,104 @@ const Tesseract = require("tesseract.js");
 const axios = require("axios");
 const fs = require("fs");
 
+// Move performOCR and deleteImage outside of the execute function
+async function performOCR(interaction, imagePath) {
+  try {
+    const { data: { text } } = await Tesseract.recognize(imagePath, "eng");
+
+    console.log(`Text: ${text}`);
+
+    // Delete the downloaded image after processing
+    deleteImage(imagePath);
+
+    // Update the interaction with the OCR result
+    await interaction.followUp({ content: text });
+  } catch (error) {
+    console.error(`Error performing OCR: ${error.message}`);
+
+    // Delete the downloaded image in case of an error
+    deleteImage(imagePath);
+
+    // Update the interaction with an error message
+    await interaction.followUp("Error performing OCR. Please try again.");
+  }
+}
+
+function deleteImage(imagePath) {
+  fs.unlink(imagePath, (err) => {
+    if (err) {
+      console.error(`Error deleting image: ${err}`);
+    } else {
+      console.log(`Image deleted successfully.`);
+    }
+  });
+}
+
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("daylist")
-    .setDescription("testing bot reading slash commands")
+    .setDescription("Testing bot reading slash commands")
+    .addStringOption((option) =>
+      option
+        .setName("daylist")
+        .setDescription("The name of the daylist.")
+        .setRequired(false)
+    )
     .addAttachmentOption((option) =>
       option
         .setName("file")
-        .setDescription("the file you wish to upload")
-        .setRequired(true)
+        .setDescription("The file you wish to upload")
+        .setRequired(false)
     ),
   async execute(interaction) {
-    // Acknowledge the interaction immediately
-    interaction.deferReply();
+    try {
+      // Acknowledge the interaction immediately
+      await interaction.deferReply();
 
-    const imagePath = "downloaded_image.png";
-    const imageUrl = interaction.options.getAttachment("file").url;
+      const hasAttachment = interaction.options.get("file") || null;
+      const daylistName = interaction.options.getString("daylist") || null;
 
-    // Download the image using axios
-    axios({
-      method: "get",
-      url: imageUrl,
-      responseType: "stream",
-    })
-      .then((response) => {
-        response.data.pipe(fs.createWriteStream(imagePath));
+      console.log("hasAttachment", hasAttachment);
+      console.log("daylistName", daylistName);
 
-        response.data.on("end", () => {
-          // Image downloaded, now perform OCR
-          performOCR(imagePath);
-        });
-      })
-      .catch((error) => {
-        console.error(`Error downloading image: ${error.message}`);
-        // Reply with an error message
-        interaction.editReply("Error downloading image. Please try again.");
-      });
+      if (hasAttachment == null && daylistName == null) {
+        // Neither option provided
+        await interaction.editReply(
+          "Please provide either an attachment or custom text."
+        );
+        return;
+      }
 
-    function performOCR(imagePath) {
-      Tesseract.recognize(imagePath, "eng", {
-        logger: (info) => console.log(info),
-      })
-        .then(({ data: { text } }) => {
-          console.log(`Text: ${text}`);
+      if (hasAttachment != null) {
+        const imagePath = "downloaded_image.png";
+        const imageUrl = interaction.options.getAttachment("file").url;
 
-          // Delete the downloaded image after processing
-          deleteImage(imagePath);
-
-          // Update the interaction with the OCR result
-          interaction.editReply(text);
+        // Download the image using axios
+        await axios({
+          method: "get",
+          url: imageUrl,
+          responseType: "stream",
         })
-        .catch((error) => {
-          console.error(`Error performing OCR: ${error.message}`);
+          .then((response) => {
+            response.data.pipe(fs.createWriteStream(imagePath));
 
-          // Delete the downloaded image in case of an error
-          deleteImage(imagePath);
-
-          // Update the interaction with an error message
-          interaction.editReply("Error performing OCR. Please try again.");
-        });
-    }
-
-    function deleteImage(imagePath) {
-      fs.unlink(imagePath, (err) => {
-        if (err) {
-          console.error(`Error deleting image: ${err}`);
-        } else {
-          console.log(`Image deleted successfully.`);
-        }
-      });
+            response.data.on("end", () => {
+              // Image downloaded, now perform OCR
+              performOCR(interaction, imagePath, daylistName);
+            });
+          })
+          .catch((error) => {
+            console.error(`Error downloading image: ${error.message}`);
+            // Reply with an error message
+            interaction.followUp("Error downloading image. Please try again.");
+          });
+      } else if (daylistName != null) {
+        // No attachment provided, just use custom text
+        await interaction.followUp(daylistName);
+      }
+    } catch (error) {
+      console.error("Error in execute:", error);
+      await interaction.followUp("An unexpected error occurred. Please check the logs.");
     }
   },
 };
