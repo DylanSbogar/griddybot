e5ff8376b7b7e99a37e048a65fea20bee43b9ad8
FR3D99 <matthewmoloney2000@gmail.com>
2024-05-03 17:01:34 +1000

    image handling for commands lookatthis and amouranth

---
+++ commands/images/uploadamouranth.js
@@ -0,0 +1,64 @@
+const { SlashCommandBuilder } = require("discord.js");
+const path = require('path');
+const fs = require('fs');
+const fetch = require('node-fetch'); // Make sure you have this package installed
+
+// Utility function to create a delay using Promises
+function wait(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('uploadamouranth')
+    .setDescription('GRRRRRRRRRRR WOOF WOOF BARK BARK ARF BARK GRRRR')
+    .addAttachmentOption(option =>
+      option
+        .setName('image')
+        .setDescription('yummy')
+        .setRequired(true)
+    ),
+  async execute(interaction) {
+    const { commandName } = interaction;
+    // Get the image attachment option
+    const imageAttachment = interaction.options.getAttachment('image');
+
+    // Get the subfolder option
+    const subfolder = 'lookatthis';
+
+    // Define the base images folder path
+    const baseImagesFolder = path.join(__dirname, '../..', 'images');
+
+    // Define the file path based on the subfolder option
+    let filePath;
+    if (subfolder) {
+      // Define the subfolder path
+      const subfolderPath = path.join(baseImagesFolder, subfolder);
+
+      // Create the subfolder if it doesn't exist 
+      fs.mkdirSync(subfolderPath, { recursive: true });
+
+      // Define the file path in the subfolder
+      filePath = path.join(subfolderPath, imageAttachment.name);
+    } else {
+      // Create the images folder if it doesn't exist
+      fs.mkdirSync(baseImagesFolder, { recursive: true });
+
+      // Define the file path in the images folder
+      filePath = path.join(baseImagesFolder, imageAttachment.name);
+    }
+
+    // Download and save the image locally
+    try {
+      const response = await fetch(imageAttachment.url);
+      const buffer = await response.buffer();
+      fs.writeFileSync(filePath, buffer);
+
+      // Send a confirmation message to the user
+      await interaction.reply(`Image saved :)`);
+    } catch (error) {
+      console.error('Error saving the image:', error);
+      await interaction.reply('An error occurred while saving the image.');
+    }
+  }
+};
