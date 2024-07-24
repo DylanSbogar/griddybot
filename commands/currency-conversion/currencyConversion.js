bb64d21c38f20d73d43acbddcd11ec9545cb0ed1
GitAYYYer <duc.h@hotmail.com>
2024-07-24 22:59:53 +1000

    add moment library and impl timezone

--- commands/currency-conversion/currencyConversion.js
+++ commands/currency-conversion/currencyConversion.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder } = require("discord.js");
 const fetch = require("node-fetch");
 const { exchangeApiKey } = require("../../config.json");
+const moment = require("moment-timezone");
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -10,14 +11,19 @@ module.exports = {
   async execute(interaction) {
     try {
       const response = await fetch(
-        `https://v6.exchangerate-api.com/v6/${exchangeApiKey}/pair/AUD/JPY`
+        `https://v6.exchangerate-api.com/v6/${process.env.EXCHANGE_API_KEY}/pair/AUD/JPY`
       );
       const data = await response.json();
 
       const rate = data.conversion_rate;
+      const lastUpdateUtc = data.time_last_update_utc;
+      const lastUpdateAest = moment(lastUpdateUtc)
+        .tz("Australia/Sydney")
+        .format("MMMM Do YYYY, h:mma");
+
       await interaction.reply(
-          `The cuwwent convewsion wate of 1 AUD to JPY is: ¥${rate}`
-        );
+        `The cuwwent convewsion wate of 1 AUD to JPY is: ¥${rate}, as of ${lastUpdateAest} AEST.`
+      );
     } catch (error) {
       console.error(error);
       await interaction.reply(
