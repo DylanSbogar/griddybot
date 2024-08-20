b9a54408807d60379ce535484afd4a6a3f59cc66
Dylan Sbogar <dylansbogar@gmail.com>
2024-08-20 20:37:50 +1000

    Converted coinFlip to ts

--- src/commands/fun/coinFlip.ts
+++ src/commands/fun/coinFlip.ts
@@ -1,7 +1,9 @@
+import { CommandInteraction } from "discord.js";
+
 const { SlashCommandBuilder } = require("discord.js");
 
 // Utility function to create a delay using Promises
-function wait(ms) {
+function wait(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
@@ -9,19 +11,19 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName("coinflip")
     .setDescription("Flip a coin!"),
-  async execute(interaction) {
+  async execute(interaction: CommandInteraction) {
     if (!interaction.isCommand()) return;
 
     if (interaction.commandName === "coinflip") {
       const coinMessage = await interaction.reply("Flipping the coin...");
       const results = ["Heads", "Tails"];
 
-      // Determine a random number of times the coin will flip.
-      const flips = Math.floor(Math.random() * (15 - 5 + 1)) + 5;
+      // Determine a random number of times the coin will flip between 5 and 15.
+      const flips: number = Math.floor(Math.random() * (15 - 5 + 1)) + 5;
 
       // Simulate animation by changing the message quickly
       for (let i = 0; i < flips; i++) {
-        await wait(250); // Adjust the delay as needed
+        await wait(250);
         await coinMessage.edit(
           `The coin is flipping... **${
             results[Math.floor(Math.random() * results.length)]
@@ -30,7 +32,7 @@ module.exports = {
       }
 
       await wait(500);
-      const result = results[Math.floor(Math.random() * results.length)];
+      const result: string = results[Math.floor(Math.random() * results.length)];
       await coinMessage.edit(`🪙 The coin landed on: **${result}** 🪙`);
     }
   },
