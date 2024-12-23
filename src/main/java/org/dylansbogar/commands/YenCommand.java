d638f889067706c2b6037c8c490bf378f43133e2
Dylan Sbogar <dylansbogar@192-168-1-113.tpgi.com.au>
2024-12-23 16:41:35 +1100

    Got date formatting working for the yen command. Will add reading from file later.

--- src/main/java/org/dylansbogar/commands/YenCommand.java
+++ src/main/java/org/dylansbogar/commands/YenCommand.java
@@ -11,36 +11,47 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 
 public class YenCommand extends ListenerAdapter {
     private static final String EXCHANGE_RATE_API_KEY = System.getenv("EXCHANGE_RATE_API_KEY");
+    private static final String url = String.format("https://v6.exchangerate-api.com/v6/%s/pair/AUD/JPY", EXCHANGE_RATE_API_KEY);
 
     @Override
     public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
         if (event.getName().equals("yen")) {
-            event.deferReply().queue();
+            event.deferReply().queue(); // Defer the reply whilst we fetch the current exchange rate.
 
-            // Fetch the current exchange rate.
             try {
-                String url = String.format("https://v6.exchangerate-api.com/v6/%s/pair/AUD/JPY", EXCHANGE_RATE_API_KEY);
-
                 HttpClient client = HttpClient.newHttpClient();
 
-                // Create an HttpRequest with the GET method
+                // Create an HttpRequest with the GET method.
                 HttpRequest request = HttpRequest.newBuilder()
                         .uri(URI.create(url))
                         .GET()
                         .build();
 
-                // Send the request and receive the response
+                // Send the request and receive the response.
                 HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
                 JSONObject responseBody = new JSONObject(response.body());
 
+                // Grab the conversion rate and round up to 2 decimal places.
                 BigDecimal conversionRate = responseBody.getBigDecimal("conversion_rate").setScale(2, RoundingMode.UP);
-                Long lastUpdated = responseBody.getLong("time_last_update_unix");
 
-                event.getHook().sendMessage(String.format("The current conversion rate of 1 AUD to JPY is: %s, as of xyz", conversionRate.toString())).queue();
+                // Grab the lastUpdated and convert it to local time zone.
+                long lastUpdated = responseBody.getLong("time_last_update_unix");
+                Instant instant = Instant.ofEpochSecond(lastUpdated);
+                ZonedDateTime localDateTime = instant.atZone(ZoneId.systemDefault());
+                DateTimeFormatter formatter = DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL);
+
+                event.getHook().sendMessage(String.format("The current conversion rate of 1 AUD to JPY is: ¥%s, as of %s", conversionRate,
+                        localDateTime.format(formatter))).queue();
             } catch (InterruptedException | IOException e) {
+                event.getHook().sendMessage("There was an error whilst fetching the current exchange rate.").queue();
                 throw new RuntimeException(e);
             }
         }
