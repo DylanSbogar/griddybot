329de9b7c0382ba923867bc57c1e02dc35f6e391
DylanSbogar <dev.dylansbogar@icloud.com>
2025-01-02 14:12:49 +1100

    Added logic to fetch emote from betterttv. Needs testing

--- src/main/java/com/dylansbogar/griddybot/commands/EmoteCommand.java
+++ src/main/java/com/dylansbogar/griddybot/commands/EmoteCommand.java
@@ -4,22 +4,16 @@ import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEve
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import net.dv8tion.jda.api.utils.FileUpload;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.json.JSONObject;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
 
 import java.io.File;
-import java.io.IOError;
 import java.io.IOException;
-import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.stream.Collectors;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class EmoteCommand extends ListenerAdapter {
     @Override
@@ -50,6 +44,7 @@ public class EmoteCommand extends ListenerAdapter {
                         }
                     }
                     event.getHook().sendMessage(String.format("Unable to fetch the %s emote locally.", emote)).queue();
+                    // fetchEmote(emote, event);
                     // TODO: Use GraphQL to fetch.
                 } else {
                     event.getHook().sendMessage("No stored emotes.").queue();
@@ -57,6 +52,28 @@ public class EmoteCommand extends ListenerAdapter {
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
+        }
+    }
+
+    private void fetchEmote(String emote, SlashCommandInteractionEvent event) {
+        String url = String.format("https://api.betterttv.net/3/emotes/shared/search?query=%s&offset=0&limit=1", emote);
+
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+
+            // Create a HttpRequest with the GET method.
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(url))
+                    .GET()
+                    .build();
+
+            // Send the request, and receive its response.
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            JSONObject responseBody = new JSONObject(response.body());
+            String emoteId = responseBody.getString("id");
+            event.getHook().sendMessage(String.format("https://cdn.betterttv.net/emote/%s/3x.webp", emoteId)).queue();
+        } catch (IOException | InterruptedException e) {
+            event.getHook().sendMessage(String.format("There was an error fetching the %s emote", emote)).queue();
         }
     }
 }
