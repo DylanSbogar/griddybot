5de339cce4e363293587097015b2fae11cd42c3c
DylanSbogar <dev.dylansbogar@icloud.com>
2025-01-02 15:58:28 +1100

    Removed OCRService

--- src/main/java/com/dylansbogar/griddybot/utils/OCRService.java
+++
@@ -1,8 +0,0 @@
-package com.dylansbogar.griddybot.utils;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class OCRService {
-//    public String extractTextFromImage(MultipartFile)
-}
