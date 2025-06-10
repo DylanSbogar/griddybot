88f38d13ed9ca555db6277c007696f4c9043a5b4
Dylan Sbogar <dylansbogar@gmail.com>
2025-06-10 15:28:52 +1000

    Adjusted the rate to 1.25/min

--- src/main/java/com/dylansbogar/griddybot/utils/OzbargainService.java
+++ src/main/java/com/dylansbogar/griddybot/utils/OzbargainService.java
@@ -24,7 +24,7 @@ public class OzbargainService {
 
     private final Map<String, Deal> dealsMap = new HashMap<>();
 
-    public static final double UPVOTES_PER_MINUTE = 0.5;
+    public static final double UPVOTES_PER_MINUTE = 1.25;
 
     public Map<String, Deal> fetchDeals() {
         try {
