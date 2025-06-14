f1c45a6a170a621ce0b7c563741c6d23df733d0c
FR3D99 <matthewmoloney2000@gmail.com>
2025-06-14 20:42:27 +1000

    save posted deals to the dealhistory table, don't post if exists in that table

---
+++ src/main/java/com/dylansbogar/griddybot/entities/PostedDeal.java
@@ -0,0 +1,22 @@
+package com.dylansbogar.griddybot.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "dealHistory")
+public class PostedDeal {
+
+    @Id
+    private String url;
+
+}
---
+++ src/main/java/com/dylansbogar/griddybot/repositories/DealHistoryRepository.java
@@ -0,0 +1,13 @@
+package com.dylansbogar.griddybot.repositories;
+
+import com.dylansbogar.griddybot.entities.PostedDeal;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface DealHistoryRepository extends JpaRepository<PostedDeal, String> {
+
+    Optional<PostedDeal> findByUrl(String url);
+}
